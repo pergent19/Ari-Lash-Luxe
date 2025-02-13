d560c40283719e17eb23e730bd201ae309d6075b
unknown <pergent.galang19@gmail.com>
2025-02-13 11:51:28 +0800

    First commit, created header and landing page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/header/navbar'
+import Landing from './components/Landing'
+function App() {
+
+  return (
+    <div className='app-container'>
+      <Navbar />
+      <Landing />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Landing.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import landing from '@/assets/images/landing.jpg';
+
+export default function Landing() {
+  return (
+    <>
+      <div
+        className="h-screen bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${landing})` }}
+      >
+        <div className="flex flex-col justify-center items-start h-full pl-10 space-y-8">
+          <p className="text-white text-[30px] sm:text-[40px] font-bold">
+            WELCOME TO THE NEW ERA
+          </p>
+          <h1 className="text-white text-[40px] sm:text-[60px] md:text-[80px] font-bold">
+            LASHES IN MINUTES, <br /> NOT HOURS
+          </h1>
+          <a
+            href="#"
+            className="inline-block rounded-md border border-transparent bg-[#C5A43B] px-8 py-3 text-center font-medium text-white hover:bg-[#9E842D]"
+          >
+            BOOK NOW
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
